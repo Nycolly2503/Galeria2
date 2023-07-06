7834fe281f305877d074bb7213a8544c1d11ca9f
Dell i5 <nycollydonascimentomendes@gmail,com>
2023-07-06 18:47:03 -0300

    atividade corrigida

---
+++ app/src/main/java/mendes/nascimento/nycolly/galeria2/MyViewHolder.java
@@ -0,0 +1,10 @@
+package mendes.nascimento.nycolly.galeria2;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView){super(itemView);}
+}
